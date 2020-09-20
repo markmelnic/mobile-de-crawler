39d97289d4eb2af4b28bd23511c27bc26fed7d65
Mark Melnic <markhappy200@gmail.com>
2020-09-20 10:59:43 +0200

    Added settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+
+# database settings
+TABLES = [
+        ("listings_links", [("links", "text")]),
+        ("processed_links", [("links", "text")]),
+        ("active_links", [("links", "text")])
+]
