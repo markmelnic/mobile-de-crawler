65c160cec89893e0de08d7e2724d378ecd6ff34b
Mark Melnic <markhappy200@gmail.com>
2020-09-20 15:45:36 +0200

    Black reformatting

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
 # database settings
 CRAWLER_DB = "crawler.sqlite3"
 TABLES = [
-        ("listings_links", [("links", "text")]),
-        ("processed_links", [("links", "text")]),
-        ("active_links", [("links", "text")])
+    ("listings_links", [("links", "text")]),
+    ("processed_links", [("links", "text")]),
+    ("active_links", [("links", "text")]),
 ]
