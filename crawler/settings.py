63d8770d115b90647eb5a709b2c389c4da9ba3c7
Mark Melnic <markhappy200@gmail.com>
2020-10-07 20:24:17 +0200

    Moved request headers to settings

--- crawler/settings.py
+++ crawler/settings.py
@@ -1,3 +1,7 @@
+# requests
+HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebkit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36"
+}
 
 # database settings
 CRAWLER_DB = "crawler.sqlite3"
